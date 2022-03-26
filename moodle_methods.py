a069a97e79900b5f691cb2eca56d1367a0f351b9
kandothshahnas <kandothshahnas@icloud.com>
2022-03-26 06:13:32 -0700

    deleted service from moodle_methods

--- moodle_methods.py
+++ moodle_methods.py
@@ -5,7 +5,6 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
