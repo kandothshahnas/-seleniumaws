a0edccb09f64c528a7025c6feb4cde60d8b3ad62
kandothshahnas <kandothshahnas@icloud.com>
2022-03-26 08:07:38 -0700

    updated selenium scripts

--- moodle_methods.py
+++ moodle_methods.py
@@ -7,6 +7,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
 
 options = Options()
 options.add_argument("--headless")
